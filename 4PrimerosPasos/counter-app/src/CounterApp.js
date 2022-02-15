134af0dd144920f7d09d6b5e771beae840760d3a
C.Manuel Zarate <camazala@gmail.com>
2022-02-14 19:00:01 -0500

    fin seccion 4

--- 4PrimerosPasos/counter-app/src/CounterApp.js
+++ 4PrimerosPasos/counter-app/src/CounterApp.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 
 const CounterApp= ( {value} ) =>{
 
+    const [counter, setCounter ] = useState(value);
+    console.log(counter );
+
+    //handleAdd
+    const handleAdd = (e) => {
+        //setCounter(counter+ 1);
+        setCounter( (c) => c+1 );//puede recibir una F con reotrno el nuevo valor que va tener ese state o counter
+    }
+
+    const handleSub = () => {
+        setCounter ( (c) => c-1 );
+    }
+
+    const reset = () => {
+        setCounter ( (c) => c=value );
+    }
+
     return (
         <>
         <h1>CounterApp</h1>
-        <h2> { value }</h2>
+        <h2> { counter }</h2>
+
+        {/* <button onClick= { (e) => { handleAdd(e) } } >+1</button> */}
+        <button onClick= { handleAdd } >+1</button>
+        <button onClick= { reset } >Reset</button>
+        <button onClick= { handleSub } >-1</button>
         </>
     )
 }
